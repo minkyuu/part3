e92922578c6666a15d08824da58f009e0dfe897e
minkyuu <minkyu023@gmail.com>
2021-08-28 16:44:55 +0900

    feat: add getAccountInfo
    About getAccountInfo(Account account) method
    
    1. Goal
    : This method is for getting account's infomation
    
    2. Input
    - account which wanna check
    
    3. Output
    - print category, accNo, owner, balance

--- BankingSystem/src/account/Account.java
+++ BankingSystem/src/account/Account.java
@@ -6,11 +6,11 @@ import java.math.BigDecimal;
 
 public class Account {
     //TODO: 일반 계좌 클래스의 속성은 계좌종류(N: 예금계좌, S:적금계좌), 계좌번호, 소유자, 잔액, 활성화 여부 5가지 입니다.
-    protected String category;             // String 계좌 종류
+    protected String category;            // String 계좌 종류
     protected String accNo;               // String 계좌번호
     protected String owner;               // String 소유자
-    protected BigDecimal balance;             // BigDecimal 잔액
-    protected boolean isActive;            // boolean 활성화 여부
+    protected BigDecimal balance;         // BigDecimal 잔액
+    protected boolean isActive;           // boolean 활성화 여부
 
     public Account() {
         //TODO: 일반 계좌의 활성화 여부를 True로, 계좌 종류를 "N"(NORMAL을 의미) 설정해줍니다.
@@ -19,14 +19,22 @@ public class Account {
     }
     public Account(String accNo, String owner, BigDecimal balance) {
         //TODO
+        this.category = "N";
         this.accNo = accNo;
         this.owner = owner;
         this.balance = balance;
+        this.isActive = true;
     }
 
     public void getAccountInfo(Account account){
         //TODO: 계좌의 기본 정보를 아래 형태로 출력해줍니다.
         //계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원
+        String category = account.getCategory();
+        String accNo = account.getAccNo();
+        String owner = account.getOwner();
+        BigDecimal balance = account.getBalance();
+
+        System.out.printf("계좌종류: %s | 계좌번호: %s | 계좌주명: %s | 잔액: %s원\n", category, accNo, owner, balance);
     }
 
     public BigDecimal withdraw(BigDecimal amount) throws Exception{
