dda5346a2f62c519d8754ca08a3eeea44e61d356
minkyuu <minkyu023@gmail.com>
2021-08-28 15:48:00 +0900

    feat: create Account Class
    About class Account
    
    1.Attributes
     - category (계좌 종류)
     - accNo (계좌번호)
     - owner
     - balance
     - isActive
    
    2. Methods
     - Account()
      : set up two attributes (isActive = true , category = "N")
    
     - Account(String accNo, String owner, BigDecimal balance)
    
     - getter/setter

--- BankingSystem/src/account/Account.java
+++ BankingSystem/src/account/Account.java
@@ -6,20 +6,23 @@ import java.math.BigDecimal;
 
 public class Account {
     //TODO: 일반 계좌 클래스의 속성은 계좌종류(N: 예금계좌, S:적금계좌), 계좌번호, 소유자, 잔액, 활성화 여부 5가지 입니다.
-    //category;             // String 계좌 종류
-    // accNo;               // String 계좌번호
-    // owner;               // String 소유자
-    // balance;             // BigDecimal 잔액
-    // isActive;            // boolean 활성화 여부
+    protected String category;             // String 계좌 종류
+    protected String accNo;               // String 계좌번호
+    protected String owner;               // String 소유자
+    protected BigDecimal balance;             // BigDecimal 잔액
+    protected boolean isActive;            // boolean 활성화 여부
 
     public Account() {
         //TODO: 일반 계좌의 활성화 여부를 True로, 계좌 종류를 "N"(NORMAL을 의미) 설정해줍니다.
+        this.isActive = true;
+        this.category = "N";
     }
     public Account(String accNo, String owner, BigDecimal balance) {
         //TODO
+        this.accNo = accNo;
+        this.owner = owner;
+        this.balance = balance;
     }
-
-    // TODO: 일반 계좌 클래스의 각 속성에 getter/setter를 제공합니다.
 
     public void getAccountInfo(Account account){
         //TODO: 계좌의 기본 정보를 아래 형태로 출력해줍니다.
@@ -34,6 +37,47 @@ public class Account {
     public BigDecimal deposit(BigDecimal amount){
         //TODO: 입금액을 받아서 입금하는 기본 메소드입니다. this를 이용해 구현해보세요.
         return amount;
+    }
+
+    // TODO: 일반 계좌 클래스의 각 속성에 getter/setter를 제공합니다.
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAccNo() {
+        return accNo;
+    }
+
+    public void setAccNo(String accNo) {
+        this.accNo = accNo;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
     }
 }
 
