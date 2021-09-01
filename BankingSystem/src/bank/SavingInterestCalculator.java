e6dee501515b23808b630cac910b7cfca0cacf99
minkyuu <minkyu023@gmail.com>
2021-09-01 15:48:49 +0900

    feat: implement getInterest() (which is in SavingInterestCalculator class)
    About getInterest(BigDecimal balance) method
    
    1. Where?
     : I implement getInterest(BigDecimal balance) at
     SavingInterestCalculator class.
    
    2. Feature
     : Customer can know interest.
     : This method and getInterest() which is in BasicInterestCalculator are slightly
     different.
    
    3. Parameter
     : balance (in Account)
    
    4. Result
     : return calculated interest
     : there are five cases according to balance.
    
     - Case1. More than 1,000,000
      : calculate the interest rate as 50%
    
     - Case2. etc
      : calculate the interest rate as 1%

--- BankingSystem/src/bank/SavingInterestCalculator.java
+++ BankingSystem/src/bank/SavingInterestCalculator.java
@@ -1,12 +1,20 @@
 package bank;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 // TODO: InterestCalculator 인터페이스 구현한 SavingInterestCalculator 클래스
-public class SavingInterestCalculator{
+public class SavingInterestCalculator implements InterestCalculator{
     public BigDecimal getInterest(BigDecimal balance) {
         BigDecimal interest;
+
         // TODO: 적금 계좌의 경우 잔액이 100만원 이상은 이자율이 50%, 그 외에는 1% 입니다.
-        return null;
+        if (balance.compareTo(new BigDecimal("1000000")) != -1) {
+            interest = balance.multiply(new BigDecimal("0.5"));
+        } else {
+            interest = balance.multiply(new BigDecimal("0.01"));
+        }
+
+        return interest.setScale(0, RoundingMode.HALF_EVEN);
     }
 }
