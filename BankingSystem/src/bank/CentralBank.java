5113efd4e34aab7d1e9875ba2bb7056caee23d95
minkyuu <minkyu023@gmail.com>
2021-09-01 17:55:16 +0900

    feat: add CentralBank class
    About CentralBank Class
    
    1. Design Pattern
     : apply the Singleton Pattern
    
    2. Attribute
     : centralbank (Singleton instance)
     : BANK_NAME
     : accountList (ArrayList Type)
    
    3. method
    - getInstance()
     : This method is for singleton pattern
    
    - getter/setter of accountList

--- BankingSystem/src/bank/CentralBank.java
+++ BankingSystem/src/bank/CentralBank.java
@@ -9,9 +9,26 @@ public class CentralBank {
     //TODO: 싱글톤 패턴으로 설계합니다.
     //TODO: accountList(Account로 이루어진 ArrayList)
     //TODO: BANK_NAME(은행명)
+    private static CentralBank centralBank = new CentralBank();
+
+    private static final String BANK_NAME = "MKBank";
+    private ArrayList<Account> accountList = new ArrayList<Account>();
+
+    private CentralBank(){}
 
     //TODO: getInstance 함수
+    public static CentralBank getInstance(){
+        if (centralBank == null)
+            centralBank = new CentralBank();
+        return centralBank;
+    }
 
     //TODO: accountList getter/setter
+    public ArrayList<Account> getAccountList() {
+        return accountList;
+    }
 
+    public void setAccountList(ArrayList<Account> accountList) {
+        this.accountList = accountList;
+    }
 }
